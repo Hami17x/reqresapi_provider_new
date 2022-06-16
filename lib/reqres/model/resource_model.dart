3ec68aaeb4e2b77c30d93564f5162826327b4730
hamix17 <hmdllh.st.17@gmail.com>
2022-06-16 20:49:09 +0300

    jsonserializable added

--- lib/reqres/model/resource_model.dart
+++ lib/reqres/model/resource_model.dart
@@ -1,26 +1,19 @@
+import 'package:json_annotation/json_annotation.dart';
+
+part 'resource_model.g.dart';
+
+@JsonSerializable(createToJson: false)
 class ResourceModel {
   List<Data>? data;
 
   ResourceModel({this.data});
 
-  ResourceModel.fromJson(Map<String, dynamic> json) {
-    if (json['data'] != null) {
-      data = <Data>[];
-      json['data'].forEach((v) {
-        data!.add(new Data.fromJson(v));
-      });
-    }
-  }
-
-  Map<String, dynamic> toJson() {
-    final Map<String, dynamic> data = new Map<String, dynamic>();
-    if (this.data != null) {
-      data['data'] = this.data!.map((v) => v.toJson()).toList();
-    }
-    return data;
+  factory ResourceModel.fromJson(Map<String, dynamic> json) {
+    return _$ResourceModelFromJson(json);
   }
 }
 
+@JsonSerializable(createToJson: false)
 class Data {
   int? id;
   String? name;
@@ -30,21 +23,7 @@ class Data {
 
   Data({this.id, this.name, this.year, this.color, this.pantoneValue});
 
-  Data.fromJson(Map<String, dynamic> json) {
-    id = json['id'];
-    name = json['name'];
-    year = json['year'];
-    color = json['color'];
-    pantoneValue = json['pantone_value'];
-  }
-
-  Map<String, dynamic> toJson() {
-    final Map<String, dynamic> data = new Map<String, dynamic>();
-    data['id'] = this.id;
-    data['name'] = this.name;
-    data['year'] = this.year;
-    data['color'] = this.color;
-    data['pantone_value'] = this.pantoneValue;
-    return data;
+  factory Data.fromJson(Map<String, dynamic> json) {
+    return _$DataFromJson(json);
   }
 }
